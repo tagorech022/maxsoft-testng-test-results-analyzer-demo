4289ab216cef4ad7adc60f2da0cf009ba82f4f15
Osanda Deshan <osanda.deshan@gmail.com>
2021-08-08 15:13:03 +0530

    Updated library to new version 1.0.2

---
+++ src/test/java/tests/ExceptionTest.java
@@ -0,0 +1,57 @@
+package tests;
+
+import com.maxsoft.testngtestresultsanalyzer.ReportListener;
+import com.maxsoft.testngtestresultsanalyzer.annotations.Category;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/**
+ * Project Name    : maxsoft-testng-test-results-analyzer-demo
+ * Developer       : Osanda Deshan
+ * Version         : 1.0.0
+ * Date            : 8/8/2021
+ * Time            : 3:09 PM
+ * Description     : This is the Exception class to simulate test exceptions
+ **/
+
+@Listeners(ReportListener.class)
+public class ExceptionTest {
+
+    @Category("Failing category 1 in ExceptionTest class")
+    @Test(description = "NullPointerException test simulation 1")
+    public void testNullPointerExceptionMethod1() {
+        String text = null;
+        System.out.println(text.equals("NullPointer"));
+    }
+
+    @Category("Failing category 1 in ExceptionTest class")
+    @Test(description = "NullPointerException test simulation 1")
+    public void testNullPointerExceptionMethod2() {
+        String text = null;
+        System.out.println(text.equals("NullPointer"));
+    }
+
+    @Category("Failing category 2 in ExceptionTest class")
+    @Test(description = "FileNotFoundException test simulation 1")
+    public void testFileNotFoundExceptionMethod1() throws IOException {
+        FileInputStream fileInputStream = new FileInputStream("./exception/exception.txt");
+        fileInputStream.available();
+    }
+
+    @Category("Failing category 2 in ExceptionTest class")
+    @Test(description = "FileNotFoundException test simulation 2")
+    public void testFileNotFoundExceptionMethod2() throws IOException {
+        FileInputStream fileInputStream = new FileInputStream("./exception/exception.txt");
+        fileInputStream.available();
+    }
+
+    @Category("Failing category 2 in ExceptionTest class")
+    @Test(description = "FileNotFoundException test simulation 3")
+    public void testFileNotFoundExceptionMethod3() throws IOException {
+        FileInputStream fileInputStream = new FileInputStream("./exception/exception.txt");
+        fileInputStream.available();
+    }
+}
